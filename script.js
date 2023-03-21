e9a108f48d19e3668d0cb28e878d579afc0cbd9a
Thomo99 <114318806+Thomo99@users.noreply.github.com>
2023-03-22 05:01:23 +1100

    grid DOM made, need to create css to prove grid works

---
+++ script.js
@@ -0,0 +1,16 @@
+document.addEventListener('DOMContentLoaded', function(){
+    const gridContainer = document.querySelector('#gridContainer');
+
+    for(let r = 0; r < 16; r++){
+        const row = document.createElement('div');
+        row.classList.add('row');
+
+        for(let c = 0; c < 16; c++){
+            const col = document.createElement('div');
+            col.classList.add('column');
+
+        }
+        row.appendChild(column);
+    }
+    gridContainer.appendChild(row);
+});
