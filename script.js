469cbec20201b2c4d47dd2f1e2f00446c8158153
Thomo99 <114318806+Thomo99@users.noreply.github.com>
2023-03-23 10:11:10 +1100

    Grid working

--- script.js
+++ script.js
@@ -4,11 +4,13 @@ document.addEventListener('DOMContentLoaded', function(){
     for(let r = 0; r < 16; r++){
         const row = document.createElement('div');
         row.classList.add('row');
+        console.log(r);
 
         for(let c = 0; c < 16; c++){
             const col = document.createElement('div');
             col.classList.add('column');
-            row.appendChild(column);
+            row.appendChild(col);
+            console.log(c);
 
         }
         gridContainer.appendChild(row);
