1277420aa7ccb3bf9d850f7bc4d8988b3dcf25d8
Thomo99 <114318806+Thomo99@users.noreply.github.com>
2023-03-23 10:20:18 +1100

    Begin making button for grid size.

--- script.js
+++ script.js
@@ -1,19 +1,23 @@
+
 document.addEventListener('DOMContentLoaded', function(){
+    const gridSizeBtn = document.querySelector('#gridSizeBtn');
+    
     const gridContainer = document.querySelector('#gridContainer');
 
     for(let r = 0; r < 16; r++){
         const row = document.createElement('div');
         row.classList.add('row');
-        console.log(r);
+        //console.log(r);//r - check
 
         for(let c = 0; c < 16; c++){
             const col = document.createElement('div');
             col.classList.add('column');
             row.appendChild(col);
-            console.log(c);
+            //console.log(c);//c - check
 
         }
         gridContainer.appendChild(row);
     }
     
 });
+
